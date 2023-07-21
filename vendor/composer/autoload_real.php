7e40af8b7d53a0dbcddf55902b34ec7d905ab569
Neetu Mittal <shabnikkigoyal@gmail.com>
2023-07-21 16:25:32 +0530

    Revert "clear git cache"
    This reverts commit af257986f4c453bc7e9bde4434f5eefd5b38f0c2.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitbf8dc242a640d7c740f283f226d0bdb4
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitbf8dc242a640d7c740f283f226d0bdb4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitbf8dc242a640d7c740f283f226d0bdb4', 'loadClassLoader'));
@@ -33,25 +31,18 @@ class ComposerAutoloaderInitbf8dc242a640d7c740f283f226d0bdb4
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitbf8dc242a640d7c740f283f226d0bdb4::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirebf8dc242a640d7c740f283f226d0bdb4($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitbf8dc242a640d7c740f283f226d0bdb4::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequirebf8dc242a640d7c740f283f226d0bdb4($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
