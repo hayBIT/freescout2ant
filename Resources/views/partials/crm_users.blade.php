465dabebfd873c20753fc26364e6968efb331929
mokkin <markus@haybach.com>
2024-04-16 21:44:23 +0200

    fix domain

--- Resources/views/partials/crm_users.blade.php
+++ Resources/views/partials/crm_users.blade.php
@@ -1,7 +1,7 @@
 <link href="{{ asset(Module::getPublicPath(AMEISE_MODULE) . '/css/style.css') }}" rel="stylesheet" type="text/css">
 @section('javascripts')
     @parent
-    <input type="hidden" id="ameise_base_url" value="{{ (config('ameisemodule.ameise_mode') == 'test' ? 'https://maklerinfo.inte.dionera.dev/' : 'https://maklerinfo.biz/') }}">
+    <input type="hidden" id="ameise_base_url" value="{{ (config('ameisemodule.ameise_mode') == 'test' ? 'https://maklerinfo.inte.dionera.dev/' : 'https://www.maklerinfo.biz/') }}">
     <script src="{{ Module::getPublicPath(AMEISE_MODULE) . '/js/crm_users.js' }}"  {!! \Helper::cspNonceAttr() !!}></script>
     <script  {!! \Helper::cspNonceAttr() !!}>
             let translations = {
