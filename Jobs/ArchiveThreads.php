7d301e8110260b071b5c20a4711f627bae5c934c
mokkin <markus@haybach.com>
2025-04-20 23:27:57 +0200

    fix typo

--- Jobs/ArchiveThreads.php
+++ Jobs/ArchiveThreads.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class ArcheiveThreads implements ShouldQueue
+class ArchiveThreads implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
